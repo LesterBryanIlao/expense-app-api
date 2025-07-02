80d9aa2e43721ea3bbd1a723cd070436f1a1157b
LesterBryanIlao <lestertetengilao@gmail.com>
2025-07-02 21:17:19 +0800

    chore: add logging to ExpenseCategoryService

--- src/main/java/teng/dev/expenseapi/controller/ExpenseController.java
+++ src/main/java/teng/dev/expenseapi/controller/ExpenseController.java
@@ -1,6 +1,5 @@
 package teng.dev.expenseapi.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
